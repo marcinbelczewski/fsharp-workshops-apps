1a0a5ebfe4fcb0cedba9507d3693afbc250145a4
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-07-25 16:31:00 +0200

    test for 12 strikes in row

--- src/bowling.tests/Tests.fs
+++ src/bowling.tests/Tests.fs
@@ -1,1 +1,11 @@
 ﻿namespace Bowling.Tests
+
+open Xunit
+
+module SpecificCases =
+    
+    [<Fact>]
+    let ``12 strikes in row`` () =
+        let expected = Some 300
+        let actual = Bowling.bowlingScore "XXXXXXXXXXXX"
+        Assert.Equal(actual, expected)

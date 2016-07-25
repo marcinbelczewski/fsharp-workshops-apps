64ba7c0355794286e86d2ebf4c02e44b79aa710f
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-07-25 11:45:11 +0200

    init

---
+++ slides/index.fsx
@@ -0,0 +1,16 @@
+﻿(**
+- title : Writing .NET applications in F#
+- description : Writing .NET applications in F#
+- author : Tomasz Heimowski
+- theme : night
+- transition : default
+
+***
+
+## F# CAMP
+
+### Writing .NET applications in F#
+
+---
+
+*)

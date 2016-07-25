c22b6982bd45a87bdecfe5c2bfa1352480fc9fba
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-07-25 16:38:15 +0200

    add Tests target to build pipeline

--- build.fsx
+++ build.fsx
@@ -1,10 +1,21 @@
 #r @"packages/Build/FAKE/tools/FakeLib.dll"
 
 open Fake
+open Fake.Testing
 
 Target "Build" (fun _ ->
     MSBuildRelease "bin" "Build" ["src/bowling.sln"]
     |> Log "Build output"
 )
 
-RunTargetOrDefault "Build"
+Target "Tests" (fun _ ->
+    ["bin/bowling.tests.dll"]
+    |> xUnit2 (fun xunitParams -> 
+        { xunitParams with ToolPath = @"packages/Build/xunit.runner.console/tools/xunit.console.exe" }
+    )
+)
+
+"Build"
+    ==> "Tests"
+
+RunTargetOrDefault "Tests"

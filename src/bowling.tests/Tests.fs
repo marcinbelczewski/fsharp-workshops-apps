311d647f96783dfaf7e9133fbcc13b8f4f17c472
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-07-25 16:40:56 +0200

    moar specific test cases

--- src/bowling.tests/Tests.fs
+++ src/bowling.tests/Tests.fs
@@ -9,3 +9,9 @@ module SpecificCases =
         let expected = Some 300
         let actual = Bowling.bowlingScore "XXXXXXXXXXXX"
         Assert.Equal(actual, expected)
+
+    [<Fact>]
+    let ``10 times 9 pins and a miss`` () =
+        let expected = Some 90
+        let actual = Bowling.bowlingScore "9-9-9-9-9-9-9-9-9-9-"
+        Assert.Equal(actual, expected)

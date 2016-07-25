6d5032338e5c1b2dd34801adbf35e0d2445b2274
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-07-25 12:06:23 +0200

    exercise - extend Digit active pattern to recognize '-' as well

--- src/bowling/Bowling.fs
+++ src/bowling/Bowling.fs
@@ -4,6 +4,8 @@ let (|Digit|_|) char =
     let zero = System.Convert.ToInt32 '0'
     if System.Char.IsDigit char then
         Some (System.Convert.ToInt32 char - zero)
+    elif char = '-' then
+        Some 0
     else
         None
 
@@ -12,7 +14,6 @@ let rec parseScore (chars: list<char>) : list<Option<int>> =
     | [] -> []
     | 'X' :: rest -> Some 10 :: parseScore rest
     | Digit x :: '/' :: rest -> Some x :: Some (10 - x) :: parseScore rest
-    | '-' :: '/' :: rest -> Some 0 :: Some 10 :: parseScore rest
     | '-' :: rest -> Some 0 :: parseScore rest
     | Digit x :: rest -> Some x :: parseScore rest
     | _ :: rest -> None :: parseScore rest

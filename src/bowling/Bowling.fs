c0ce3813d707c6a07fb1730a4b4ac87c51c8021f
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-07-25 12:04:57 +0200

    place 'Digit' active pattern and 'parseScore' function

--- src/bowling/Bowling.fs
+++ src/bowling/Bowling.fs
@@ -1,1 +1,18 @@
 ﻿module Bowling
+
+let (|Digit|_|) char =
+    let zero = System.Convert.ToInt32 '0'
+    if System.Char.IsDigit char then
+        Some (System.Convert.ToInt32 char - zero)
+    else
+        None
+
+let rec parseScore (chars: list<char>) : list<Option<int>> =
+    match chars with
+    | [] -> []
+    | 'X' :: rest -> Some 10 :: parseScore rest
+    | Digit x :: '/' :: rest -> Some x :: Some (10 - x) :: parseScore rest
+    | '-' :: '/' :: rest -> Some 0 :: Some 10 :: parseScore rest
+    | '-' :: rest -> Some 0 :: parseScore rest
+    | Digit x :: rest -> Some x :: parseScore rest
+    | _ :: rest -> None :: parseScore rest

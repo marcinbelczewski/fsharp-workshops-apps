87b6e019abd6662bc0b3fb4879bfbc4bf567a196
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-07-26 12:00:31 +0200

    hello world suave

--- src/bowling.web/Program.fs
+++ src/bowling.web/Program.fs
@@ -1,7 +1,3 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open Suave
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+startWebServer defaultConfig (Successful.OK "Hello world")
